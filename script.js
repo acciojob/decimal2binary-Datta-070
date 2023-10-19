1a651641536dd9e8fc510e74c13fd572b8757731
Chinmay Datt D <chinmaydatt003@gmail.com>
2023-10-19 15:44:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
-function decimalToBinary(num) {
+function decimalToBinary(decimalNumber) {
   //Write you code here
-	let op="";
-	while(num >= 2){
-		let reminder=num%2;
-		op=reminder+op;
-		num=parseInt(num/2);
-	}
-	if(op===1){
-		op=1+op;
-	}
-	console.log(op);
+	if (isNaN(decimalNumber) || decimalNumber < 0 || Math.floor(decimalNumber) !== decimalNumber) {
+        return "Invalid input. Please enter a non-negative integer.";
+    }
+
+    // Handle the special case of 0 separately
+    if (decimalNumber === 0) {
+        return "0";
+    }
+
+    // Initialize an empty string to store the binary representation
+    let binaryNumber = "";
+
+    // Convert decimal to binary
+    while (decimalNumber > 0) {
+        // Append the remainder to the binary representation
+        binaryNumber = (decimalNumber % 2) + binaryNumber;
+
+        // Update the decimal number by dividing it by 2
+        decimalNumber = Math.floor(decimalNumber / 2);
+    }
+
+    return binaryNumber;
 }
